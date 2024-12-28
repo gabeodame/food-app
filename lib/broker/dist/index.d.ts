4d6f9d6ded4170aaab5b3df6a9e88267d5fbd7f5
Gabriel Odame <66753945+gabeodame@users.noreply.github.com>
2024-12-28 15:08:09 -0500

    complete deadletter message logic for userprofile

--- lib/broker/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { default as RabbitMQBroker } from "./broker";
-export * from "./types";
--- services/profile/src/lib/rabbitmq-consumer.service.ts
+++ services/profile/src/lib/rabbitmq-consumer.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import RabbitMQBroker from './broker';
+import { RabbitMQBroker } from '@anchordiv/rabbitmq-broker';
 
 @Injectable()
 export class RabbitMQConsumerService {
