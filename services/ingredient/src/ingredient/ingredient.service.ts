4cddfa09424ea694f5484f130ab0756a5b63561b
Gabriel Odame <66753945+gabeodame@users.noreply.github.com>
2025-01-03 02:19:37 -0500

    Initializes message queues from ingredient to recipe

--- services/ingredient/src/ingredient/ingredient.service.ts
+++ services/ingredient/src/ingredient/ingredient.service.ts
@@ -4,6 +4,8 @@ import { Repository } from 'typeorm';
 import { Ingredient } from '../lib/ingredient.entity';
 import { RabbitMQBroker } from '@anchordiv/rabbitmq-broker';
 
+type Type = 'topic' | 'direct' | 'fanout' | 'headers';
+
 @Injectable()
 export class IngredientService {
   private rabbitMQUrl: string;
@@ -17,18 +19,9 @@ export class IngredientService {
   async createIngredient(data: Partial<Ingredient>): Promise<Ingredient> {
     const ingredient = this.ingredientRepo.create(data);
 
-    // Initialize RabbitMQ broker and publish ingredient-created event
-    const broker = RabbitMQBroker.getInstance();
-    await broker.init(this.rabbitMQUrl);
+    // Publish the ingredient creation event
+    await this.handlePublishOrUpdateIngredient('create', ingredient);
 
-    // Publish ingredient creation event to the exchange
-    const exchange = 'recipe.ingredients.inventory-updates';
-    const routingKey = 'ingredients.create.new-ingredient';
-    const message = JSON.stringify(data);
-    const type = 'topic';
-    await broker.publishToExchange(exchange, routingKey, message, type, {
-      persistent: true,
-    });
     return this.ingredientRepo.save(ingredient);
   }
 
@@ -45,10 +38,37 @@ export class IngredientService {
     data: Partial<Ingredient>,
   ): Promise<Ingredient> {
     await this.ingredientRepo.update({ id }, data);
+
+    // Publish the ingredient update event
+    await this.handlePublishOrUpdateIngredient('update', data);
     return this.getIngredientById(id);
   }
 
   async deleteIngredient(id: string): Promise<void> {
     await this.ingredientRepo.delete({ id });
+  }
+
+  // Business for handling published messages to the exchange
+  async handlePublishOrUpdateIngredient(
+    action: string = 'create',
+    data: Partial<Ingredient>,
+    exchange: string = 'recipe.ingredients.inventory-updates',
+    routingKey: string = 'ingredients.create.ingredient',
+    type: Type = 'topic',
+  ): Promise<void> {
+    // Initialize RabbitMQ broker and publish ingredient-created event
+    const broker = RabbitMQBroker.getInstance();
+    await broker.init(this.rabbitMQUrl);
+
+    const message = JSON.stringify(data);
+
+    if (action) {
+      routingKey = `recipe.ingredients.${action}.ingredient`;
+    }
+
+    // Publish ingredient creation event to the exchange
+    await broker.publishToExchange(exchange, routingKey, message, type, {
+      persistent: true,
+    });
   }
 }
