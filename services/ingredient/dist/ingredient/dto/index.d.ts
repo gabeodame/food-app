48417def6139ee4663af3abe9add4433e4ac0919
Gabriel Odame <gabriel.odame@anchordiv.com>
2026-01-03 21:38:15 -0500

    chore: remove tracked deps/build/coverage and key material

--- services/ingredient/dist/ingredient/dto/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './create-ingredient.dto';
-export * from './update-ingredient.dto';
-export * from './ingredient-created.dto';
--- services/profile/dist/app.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-export declare const getOrmConfig: (configService: ConfigService) => TypeOrmModuleOptions;
-export declare class AppModule {
-}
