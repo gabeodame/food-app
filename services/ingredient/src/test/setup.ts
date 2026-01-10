2a1706f589ce892df0c9f927c8497f425889acd0
Gabriel Odame <gabriel.odame@anchordiv.com>
2026-01-10 18:42:10 -0500

    implementing fix deployment bug

--- services/ingredient/src/test/setup.ts
+++ services/ingredient/src/test/setup.ts
@@ -1,8 +1,10 @@
-const bufferModule = require("buffer") as {
-  Buffer: typeof Buffer;
+import { Buffer, SlowBuffer as ImportedSlowBuffer } from "buffer";
+
+const SlowBuffer = ImportedSlowBuffer ?? Buffer;
+const globalBuffer = globalThis as typeof globalThis & {
+  Buffer?: typeof Buffer;
   SlowBuffer?: typeof Buffer;
 };
 
-if (!bufferModule.SlowBuffer) {
-  bufferModule.SlowBuffer = bufferModule.Buffer;
-}
+globalBuffer.Buffer = Buffer;
+globalBuffer.SlowBuffer = SlowBuffer;
