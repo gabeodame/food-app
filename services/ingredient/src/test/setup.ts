e6f1bcbd83f8b2b2b2430dd35735f12e3bd328ca
Gabriel Odame <gabriel.odame@anchordiv.com>
2026-01-12 19:39:43 -0500

    test: fix ingredient setup buffer shim

--- services/ingredient/src/test/setup.ts
+++ services/ingredient/src/test/setup.ts
@@ -1,10 +1,8 @@
-import { Buffer, SlowBuffer as ImportedSlowBuffer } from "buffer";
-
-const SlowBuffer = ImportedSlowBuffer ?? Buffer;
-const globalBuffer = globalThis as typeof globalThis & {
-  Buffer?: typeof Buffer;
+const bufferModule = require("buffer") as {
+  Buffer: typeof Buffer;
   SlowBuffer?: typeof Buffer;
 };
 
-globalBuffer.Buffer = Buffer;
-globalBuffer.SlowBuffer = SlowBuffer;
+if (!bufferModule.SlowBuffer) {
+  bufferModule.SlowBuffer = bufferModule.Buffer;
+}
